5ba07ebac94108f25e4087a593d82564b015cbfe
Keegan-Smi <smithk2736@student.faytechcc.edu>
2026-02-22 17:35:51 -0500

    Add files via upload

---
+++ P1LAB1_Keegan_Smith.py
@@ -0,0 +1,14 @@
+# Keegan Smith
+# February 22, 2026
+# Welcome Program – CTI-110
+# This program asks the user for their first and last name and displays a welcome message.
+
+# Prompt the user for their first name
+first_name = input("Enter your first name: ")
+
+# Prompt the user for their last name
+last_name = input("Enter your last name: ")
+
+# Display welcome message
+print("Hello" + ",", first_name, last_name + "!", "Welcome to CTI-110.")
+
